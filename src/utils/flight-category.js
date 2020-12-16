de80e4224c9e2ff7efa8096f65e71ea2607caad0
Zach Graves <zagraves@gmail.com>
2020-12-16 11:49:42 -0800

    rework flight categories

--- src/utils/flight-category.js
+++ src/utils/flight-category.js
@@ -3,18 +3,25 @@ import * as math from 'mathjs';
 import { sprintf } from 'sprintf-js';
 
 // Categories are ordered in ascending priority. 
+// All categories have math expressions that are 
+// handled as this `OR` that to determine a match.
 const categories = config.get('flight-categories');
-const layers = config.get('base-layer-codes');
+const baseLayers = config.get('base-layer-codes');
 
 export function get(metar) {
-  const search = (vis, base) => (cat) => math.evaluate(cat.expression.map(exp => sprintf(exp, vis, base))).some(Boolean);
-  const clouds = metar.clouds.filter(cloud => layers.includes(cloud.code));
+  const search = (context) => 
+    (cat) => math.evaluate(cat.expression.map(exp => sprintf(exp, context))).some(Boolean);
 
   // Find lowest cloud bases (bkn,ovc,vv) below 12000 AGL
-  const bases = clouds.length ? Math.min(...clouds.map(cloud => cloud.base_feet_agl)) : 12000;
+  const clouds = (metar.clouds || [])
+    .filter(cloud => baseLayers.includes(cloud.code))
+    .map(cloud => cloud.base_feet_agl);
+
+  const cloud_base = clouds.length ? Math.min(...clouds) : 12000;
   const visibility = metar.visibility.miles_float;
 
-  return categories.filter(search(visibility, bases)).slice(-1).shift();
+  // Return first matching category.
+  return categories.filter(search({ visibility, cloud_base })).slice(-1).shift();
 }
 
 export default { get };
