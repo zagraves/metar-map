41b51cb687fce374cff624aca0cb4e301ceca406
Zach Graves <zagraves@gmail.com>
2020-12-16 10:43:11 -0800

    tweaks

--- src/utils/metar.js
+++ src/utils/metar.js
@@ -3,32 +3,41 @@ import URL from 'url';
 import axios from 'axios';
 import parser from 'aewx-metar-parser';
 import cheerio from 'cheerio';
+import { sprintf } from 'sprintf-js';
 
 const source = config.get('metar-source');
 
 async function get(stations) {
   const codes = stations.map(s => s.toUpperCase());
-  const path = `/metar/data?ids=${codes}&format=${source.format}&date=&hours=0`;
-  const url = URL.resolve(source.host, path);
+  const url = URL.resolve(source.host, sprintf(source.path, codes));
 
-  // We use aviationweather.gov to scrape html, instead
-  // of using the xml api. This should allow swapping the source url
+  // We'll scrape html, instead of using a more specific API. 
+  // This should allow swapping the source url/path in the config
   // to anything else that might output raw METAR strings in the future.
-  const response = await axios.get(url);
+  const response = await axios.get(url, { headers: source.headers });
   const $ = cheerio.load(response.data);
 
-  return codes.map((id) => {
-    const element = $(source.selector)
-      .filter((i, el) => $(el).text().match(id))
-      .first();
+  // Find the first matching metar for the given station.
+  // 1. Use the station id to match the desired string. 
+  // e.g. "KRNT 151853Z 15006KT 2 1/2SM -RA ...".match(/^KRNT/)
+  // 2. Change the selector in `config/default.json` to update 
+  // how it queries the document for the metar string. 
+  const scrape = (stationId) => 
+    $(source.selector).filter((i, el) => $(el).text().match(stationId)).first();
+
+  return codes
+    .map((id) => {
+      const element = scrape(id);
       
-    if (!element.text()) return false;
+      // Skip missing result
+      if (!element.text()) return false;
 
-    const metar = element.text();
-    const decoded = parser(metar);
+      const metar = element.text();
+      const decoded = parser(metar);
 
-    return { id, metar, decoded };
-  }).filter(Boolean);
+      return { id, metar, decoded };
+    })
+    .filter(Boolean);
 }
 
 
