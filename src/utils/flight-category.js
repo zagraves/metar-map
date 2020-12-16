ece08a9dd3a9d8e29c4ab7d854c7e21691578780
Zach Graves <zagraves@gmail.com>
2020-12-16 10:18:43 -0800

    Update flight-category.js

--- src/utils/flight-category.js
+++ src/utils/flight-category.js
@@ -9,9 +9,10 @@ const layers = config.get('base-layer-codes');
 export function get(metar) {
   const search = (vis, base) => (cat) => math.evaluate(cat.expression.map(exp => sprintf(exp, vis, base))).some(Boolean);
   const clouds = metar.clouds.filter(cloud => layers.includes(cloud.code));
+
+  // Find lowest cloud bases (bkn,ovc,vv) below 12000 AGL
   const bases = clouds.length ? Math.min(...clouds.map(cloud => cloud.base_feet_agl)) : 12000;
   const visibility = metar.visibility.miles_float;
-
   return categories.filter(search(visibility, bases)).slice(-1).shift();
 }
 
