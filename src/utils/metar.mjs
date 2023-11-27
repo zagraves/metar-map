d7e9b3af435ea18b17038c70a2dda0a4d6cc21ed
zach.graves <zach.graves@onxmaps.com>
2023-11-27 10:19:15 -0700

    fixes

--- src/utils/metar.mjs
+++ src/utils/metar.mjs
@@ -1,9 +1,7 @@
-import config from 'config';
-import URL from 'url';
 import axios from 'axios';
 import parser from 'aewx-metar-parser';
-import cheerio from 'cheerio';
 import format from 'sprintf-js';
+import config from 'config';
 
 const source = config.get('metar-source');
 
@@ -15,29 +13,15 @@ async function get(stations) {
   // This should allow swapping the source url/path in the config
   // to anything else that might output raw METAR strings in the future.
   const response = await axios.get(url, { headers: source.headers });
-  const $ = cheerio.load(response.data);
 
-  // Find the first matching metar for the given station.
-  // 1. Use the station id to match the desired string. 
-  // e.g. "KRNT 151853Z 15006KT 2 1/2SM -RA ...".match(/^KRNT/)
-  // 2. Change the selector in `config/default.json` to update 
-  // how it queries the document for the metar string. 
-  const scrape = (stationId) => 
-    $(source.selector).filter((i, el) => $(el).text().match(stationId)).first();
+  const metarLines = response.data.split("\n").filter(Boolean);
 
-  return codes
-    .map((id) => {
-      const element = scrape(id);
-      
-      // Skip missing result
-      if (!element.text()) return false;
+  return metarLines.map((metar) => {
+    const id = metar.split(' ')[0];
+    const decoded = parse(metar);
 
-      const metar = element.text();
-      const decoded = parse(metar);
-
-      return { id, metar, decoded };
-    })
-    .filter(Boolean);
+    return {id, metar, decoded}
+  });
 }
 
 export function parse(metar) {
